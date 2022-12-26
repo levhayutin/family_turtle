5827536b85fef3ee39c61cac2cf8e371f084d07a
Lev Hayutin <levhayutin@gmail.com>
2022-12-25 18:20:23 -0700

    start of wes
    Signed-off-by: Lev Hayutin <levhayutin@gmail.com>

---
+++ src/wes.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import turtle
+import time
+
+letter_x = 800
+letter_y = 100
+letter_width = 50
+
+# setup turtle screen and turtle
+c = turtle.Turtle()
+s = turtle.Screen()
+
+# setup background
+s.bgcolor("yellow")
+
+# get to the initial position
+c.penup()
+c.setx(-800)
+c.sety(0)
+
+c.pd()
+c.fd(10)
+c.pu()
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.color("cyan")
+c.lt(90)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.rt(45)
+c.rt(45)
+c.rt(45)
+c.rt(45)
+c.lt(45)
+c.fd(100)
+c.bk(100)
+c.pd()
+c.fd(100)
+c.fd(100)
+c.color("yellow")
+c.bk(100)
+c.bk(100)
+c.color("cyan")
+c.rt(10)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.color("yellow")
+c.bk(100)
+c.bk(100)
+c.color("cyan")
+c.fd(100)
+c.lt(90)
+c.fd(100)
+c.fd(100)
+c.rt(90)
+c.fd(100)
+c.fd(100)
+c.lt(100)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+c.fd(100)
+ 
